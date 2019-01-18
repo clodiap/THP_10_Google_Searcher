16e399dc41361eb80527aad6df115669ac5e1e7a
Clodiap <clodiapp@gmail.com>
2019-01-18 17:02:23 +0100

    google_searcher

---
+++ lib/google_searcher.rb
@@ -0,0 +1,21 @@
+# require 'watir'
+require 'launchy'
+
+# Launchy.open("http://stackoverflow.com")
+
+#search?q=ta+recherche
+
+def check_if_user_gave_input
+  abort("missing input") if ARGV.empty?
+end
+
+def get_url
+  return url = ARGV
+end
+
+def open_url(url)
+  # p url
+  Launchy.open("https://www.google.com/search?q=#{url.join("+")}")
+end
+check_if_user_gave_input
+open_url(get_url)
