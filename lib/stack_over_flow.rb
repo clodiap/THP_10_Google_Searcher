4157c02e2c99408a8d20498ccbf38f8dc0094c70
Clodiap <clodiapp@gmail.com>
2019-01-19 19:27:05 +0100

    ajout d'un script pour stackoverflow

---
+++ lib/stack_over_flow.rb
@@ -0,0 +1,16 @@
+require 'launchy'
+
+def check_if_user_gave_input
+  abort("Missing input, please enter your search words.") if ARGV.empty?
+end
+
+def get_url
+  return url = ARGV
+end
+
+def open_url(url)
+  # p url
+  Launchy.open("https://stackoverflow.com/search?q=#{url.join("+")}")
+end
+check_if_user_gave_input
+open_url(get_url)
